2470160d02b70748b97f392f2aa36ed8e8b6568a
天方夜谭 <tianfangyetan01@qq.com>
2026-02-17 22:51:24 +0800

    perf: 优化
    Signed-off-by: 天方夜谭 <tianfangyetan01@qq.com>

--- src/apps/com.microsoft.office.outlook.ts
+++ src/apps/com.microsoft.office.outlook.ts
@@ -8,22 +8,22 @@ export default defineGkdApp({
       key: 0,
       name: '分段广告-收件箱顶部广告',
       fastQuery: true,
-      activityIds: ['com.acompli.acompli.CentralActivity'],
+      activityIds: 'com.acompli.acompli.CentralActivity',
       rules: [
         {
           key: 0,
+          fastQuery: true,
           matches:
-            '[vid="ad_right_container"] >2 [vid="ad_choices_container"][visibleToUser=true] > ImageButton',
-          snapshotUrls: ['https://i.gkd.li/i/25366102'],
-          exampleUrls: [
-            'https://e.gkd.li/b1354900-bc3f-4925-a9d0-a56fd46b4b2d',
-          ],
+            '@ImageButton[clickable=true] <n [vid="ad_choices_container"] <n * < [vid="ad_right_container"]',
+          snapshotUrls: 'https://i.gkd.li/i/25366102',
+          exampleUrls: 'https://e.gkd.li/b1354900-bc3f-4925-a9d0-a56fd46b4b2d',
         },
         {
           key: 1,
           preKeys: [0],
+          fastQuery: true,
           matches:
-            '@LinearLayout[clickable=true] >2 [vid="title"][text="Hide ad" || text="隐藏广告"][visibleToUser=true]',
+            '@[clickable=true] >2 [text="Hide ad" || text="隐藏广告"]',
           snapshotUrls: ['https://i.gkd.li/i/25366119'],
         },
       ],
