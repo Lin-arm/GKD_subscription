31521e5699b36030be4bd89ed0b081ab3ba30ce6
ezn24 <eznotsimple@gmail.com>
2026-03-09 19:07:10 +1100

    feat: 熊猫外卖-全屏广告-弹窗广告

---
+++ src/apps/com.hungrypanda.waimai.ts
@@ -0,0 +1,29 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.hungrypanda.waimai',
+  name: '熊猫外卖',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          activityIds:
+            'com.haya.app.pandah4a.ui.sale.home.container.HomeContainerActivity',
+          matches: [
+            '[vid="iv_home_ad_close" || vid="iv_close"][clickable=true][visibleToUser=true]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/25813448',
+            'https://i.gkd.li/i/25822079',
+            'https://i.gkd.li/i/25822081',
+            'https://i.gkd.li/i/25813449',
+          ],
+        },
+      ],
+    },
+  ],
+});
