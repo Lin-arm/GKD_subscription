94b2dc4f17d09c0e0ae98cc994a48601ae942dbb
ezn24 <eznotsimple@gmail.com>
2026-03-15 00:02:00 +1100

    feat: 熊猫外卖-全屏广告-弹窗广告 结算弹窗广告

--- src/apps/com.hungrypanda.waimai.ts
+++ src/apps/com.hungrypanda.waimai.ts
@@ -11,14 +11,17 @@ export default defineGkdApp({
         {
           key: 0,
           fastQuery: true,
-          activityIds:
+          activityIds: [
             'com.haya.app.pandah4a.ui.sale.home.container.HomeContainerActivity',
+            'com.haya.app.pandah4a.ui.pay.success.order.PaySuccessActivity',
+          ],
           matches: [
             '[vid="iv_home_ad_close" || vid="iv_close"][clickable=true][visibleToUser=true]',
           ],
           snapshotUrls: [
             'https://i.gkd.li/i/25813448', // iv_home_ad_close
             'https://i.gkd.li/i/25822079', // iv_home_ad_close
+            'https://i.gkd.li/i/25995791', // iv_home_ad_close
             'https://i.gkd.li/i/25813449', // iv_close
           ],
         },
