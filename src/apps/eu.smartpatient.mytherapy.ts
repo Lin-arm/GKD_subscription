45d3abd165e965f2c63c32f271ff9f8544842efc
cjy0812 <cjy081219@Gmail.com>
2026-03-25 02:41:29 +0800

    feat: MyTherapy 功能类-快速添加按需药物

--- src/apps/eu.smartpatient.mytherapy.ts
+++ src/apps/eu.smartpatient.mytherapy.ts
@@ -29,5 +29,28 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-快速添加按需药物',
+      desc: '添加一次性条目-按需要(第一个)',
+      activityIds: '.feature.mainactivity.MainActivity',
+      actionMaximum: 1,
+      rules: [
+        {
+          key: 0,
+          matches:
+            'ScrollView >2 @[clickable=true] > [id="logOneTimeEntry"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/26223457',
+          exampleUrls: 'https://e.gkd.li/6cef22b5-89f0-4a39-ab01-4df740d1556b',
+        },
+        {
+          preKeys: [0],
+          matches:
+            'ScrollView > TextView + @View[clickable=true] > TextView[visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/26223492',
+          exampleUrls: 'https://e.gkd.li/a1fa2f0b-a011-42bc-b6f4-d520007cf036',
+        },
+      ],
+    },
   ],
 });
