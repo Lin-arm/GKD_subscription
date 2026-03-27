0dc10e541c5463257685cda9029e875e018a770f
cjy0812 <cjy081219@Gmail.com>
2026-03-28 01:04:23 +0800

    feat: 云·原神 开屏广告-自动下载版本更新

--- src/apps/com.miHoYo.cloudgames.ys.ts
+++ src/apps/com.miHoYo.cloudgames.ys.ts
@@ -5,6 +5,34 @@ export default defineGkdApp({
   name: '云·原神',
   groups: [
     {
+      key: 0,
+      name: '开屏广告-自动下载版本更新',
+      desc: '强制更新否则不让玩(本质和开屏广告性质一样)',
+      matchTime: 16000,
+      actionMaximum: 2,
+      resetMatch: 'app',
+      fastQuery: true,
+      activityIds: 'com.mihoyo.cloudgame.main.MiHoYoCloudMainActivity',
+      rules: [
+        {
+          key: 0,
+          name: '点击[立即更新]',
+          matches:
+            '[vid="mUpgradeTitle"][visibleToUser=true] +3 LinearLayout > [vid="mUpgradeDialogOK"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/26310355',
+          exampleUrls: 'https://e.gkd.li/c061f2fd-ed19-4ae2-84c9-ae1bbc37ace1',
+        },
+        {
+          preKeys: [0],
+          name: '点击[开始安装]',
+          matches:
+            'ViewGroup[childCount=3] > [vid="mDescription"][visibleToUser=true] + [vid="mBtnConfirm"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/26310561',
+          exampleUrls: 'https://e.gkd.li/c5f4db25-e1e5-492e-874d-c4e96eea5e99',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '功能类-自动点击[使用流量进行游戏]',
       rules: [
