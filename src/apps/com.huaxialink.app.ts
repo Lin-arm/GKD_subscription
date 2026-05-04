0bec88a13edf8a35f070f987ad87ddabe2346abf
cjy0812 <cjy081219@Gmail.com>
2026-05-05 05:40:59 +0800

    feat: 华夏直通 功能类-自动关闭首页提示

--- src/apps/com.huaxialink.app.ts
+++ src/apps/com.huaxialink.app.ts
@@ -21,5 +21,23 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-自动关闭首页提示',
+      desc: '关闭每次启动显示"不支持无缝切换"',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.MainActivity',
+          matches:
+            '[text*="暂不支持服务器间无缝切换"] <<3 * + [vid="buttonPanel"] >2 [text="我知道了"]',
+          snapshotUrls: 'https://i.gkd.li/i/27377453',
+          exampleUrls: 'https://e.gkd.li/0ef30ad2-9ba0-4b3e-a47d-241bb161ec6e',
+        },
+      ],
+    },
   ],
 });
