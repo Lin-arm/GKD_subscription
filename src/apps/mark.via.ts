9faeadc9b682849cb16fc10153c49c97e47d4e09
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-08-09 11:46:54 +0800

    perf: 不使用随机vid ; 适配其它语言

--- src/apps/mark.via.ts
+++ src/apps/mark.via.ts
@@ -6,14 +6,21 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '功能类-下载确认',
-      desc: '自动点击下载弹窗中的确定按钮',
-      fastQuery: true,
+      name: '功能类-自动[确认]下载',
+      desc: '自动点击下载弹窗中的[确定]按钮',
       rules: [
         {
-          activityIds: 'mark.via.Shell',
-          matches: '[vid="ed" || vid="eo"]',
-          snapshotUrls: 'https://i.gkd.li/i/30865110',
+          fastQuery: true,
+          activityIds: '.Shell',
+          matches:
+            '@[text="确定" || text="確定" || text="OK"] -n [text*="下载" || text*="下載" || text*="download"][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/30865110', //确定
+            'https://i.gkd.li/i/30867153', //确定
+            'https://i.gkd.li/i/30867205', //確定
+            'https://i.gkd.li/i/30867206', //OK
+          ],
+          exampleUrls: 'https://e.gkd.li/2f2ef3bf-019d-4936-bbd8-51ba4cbe698c',
         },
       ],
     },
